fa75c92b9f1c3332ea3a94543acc1d2fe7368eef
imrofayel <adamrofayel@gmail.com>
2026-03-09 18:24:46 +0500

    feat: update user display logic and add GitHubUser interface

--- app/app.vue
+++ app/app.vue
@@ -3,7 +3,7 @@
     <h1>Nuxt Auth Example</h1>
 
     <div v-if="loggedIn">
-      <p>Welcome, {{ displayName }}!</p>
+      <p>Welcome, {{ info.username }}!</p>
       <button @click="logout">Logout</button>
     </div>
     <div v-else>
@@ -15,13 +15,7 @@
 <script setup lang="ts">
 const { loggedIn, user, clear } = useUserSession();
 
-const displayName = computed(() => {
-  const currentUser = user.value as Record<string, unknown> | undefined;
-  return (
-    (currentUser?.username as string | undefined) ||
-    "GitHub User"
-  );
-});
+const info = computed(() => user.value as GitHubUser);
 
 const login = async () => {
   await navigateTo("/auth/github", { external: true });
---
+++ shared/types/user.ts
@@ -0,0 +1,6 @@
+export interface GitHubUser {
+  username: string;
+  name: string;
+  email: string;
+  avatar: string;
+}
